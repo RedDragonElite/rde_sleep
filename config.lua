12c7b3d7362b0d2f41e71c599a67fea8245eb013
△ ᛋᛅᚱᛒᛅᚾᛏᛋ ᛒᛁᛏᛅ  ▽ <57282916+RedDragonElite@users.noreply.github.com>
2026-04-13 00:04:49 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 -- ============================================
--- 🐉 RDE SLEEPMOD - CONFIG v1.0.1
+-- 🐉 RDE SLEEPMOD - CONFIG v1.0.2
 -- Proximity Loading | Statebag Sync | ox_core
 -- Author: Red Dragon Elite | SerpentsByte
 -- ============================================
